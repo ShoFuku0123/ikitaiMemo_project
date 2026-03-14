2fcc2ac02b209f815ffb4c418c70b1db5075f4ac
ShoFukuyama <sho.fukuyama0123@gmail.com>
2026-03-14 12:30:48 +0900

    fix(web): initialize sqflite for web using sqflite_common_ffi_web

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -1,5 +1,8 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:flutter/foundation.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
@@ -14,6 +17,26 @@ class DatabaseHelper {
   }
 
   Future<Database> initDatabase() async {
+    if (kIsWeb) {
+      // For web, use the sqflite_common_ffi_web factory
+      var factory = databaseFactoryFfiWeb;
+      return await factory.openDatabase(
+        'store.db',
+        options: OpenDatabaseOptions(
+          version: 1,
+          onCreate: (db, version) async {
+            await db.execute('''
+          CREATE TABLE stores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            prefecture TEXT,
+            storeName TEXT
+          )
+        ''');
+          },
+        ),
+      );
+    }
+
     String path = join(await getDatabasesPath(), 'store.db');
 
     return await openDatabase(
